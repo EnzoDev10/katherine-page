588a201848671fa00e644f51450a69051f54efa5
Enzo Cañete <enzodev4@gmail.com>
2024-09-11 19:42:16 -0300

    change a folder name

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ function toggleMenu() {
 	}
 
 	// Temporally disables scrolling on the page
-	document.body.classList.toggle('stop-scrolling');
+	// Get the current page scroll position
+	scrollTop = window.scrollY || document.documentElement.scrollTop;
+	(scrollLeft = window.scrollX || document.documentElement.scrollLeft),
+		// if any scroll is attempted,
+		// set this to the previous value
+		(window.onscroll = function () {
+			window.scrollTo(scrollLeft, scrollTop);
+		});
+
+	function enableScroll() {
+		window.onscroll = function () {};
+	}
+	//document.body.classList.toggle('stop-scrolling');
 
 	menu.classList.toggle('active');
 	menuImg.classList.toggle('active');
