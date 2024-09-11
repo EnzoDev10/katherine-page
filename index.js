bb66c30007a65590f9bcb574e317b52835a9fe02
Enzo Cañete <enzodev4@gmail.com>
2024-09-11 16:01:32 -0300

    Added menu on mobile sizes

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 let menuBtn = document.querySelector('.menu-btn');
-let closeBtn = document.querySelector('.close-btn')
-let menu = document.querySelector('.menu')
+let menuImg = document.querySelector('.menu-img');
+
+let menu = document.querySelector('.menu');
+let overlay = document.querySelector('.overlay');
 
 function toggleMenu() {
-    menu.classList.toggle('active')
+	// Changes between images depending on the state of the menu
+	if (!menuImg.classList.contains('active')) {
+		menuImg.src = 'images\\close.svg';
+	} else {
+		menuImg.src = 'images\\menu.png';
+	}
+
+	// Temporally disables scrolling on the page
+	document.body.classList.toggle('stop-scrolling');
+
+	menu.classList.toggle('active');
+	menuImg.classList.toggle('active');
+
+	overlay.classList.toggle('active');
 }
 
 menuBtn.addEventListener('click', toggleMenu);
-closeBtn.addEventListener('click', toggleMenu);
