440de7536b5c63362f110f132f0878272786bb2a
Enzo Cañete <enzodev4@gmail.com>
2024-09-11 19:51:10 -0300

    added stop scrolling to the menu

--- index.js
+++ index.js
@@ -12,20 +12,7 @@ function toggleMenu() {
 		menuImg.src = 'images\\icons\\menu-icon.png';
 	}
 
-	// Temporally disables scrolling on the page
-	// Get the current page scroll position
-	scrollTop = window.scrollY || document.documentElement.scrollTop;
-	(scrollLeft = window.scrollX || document.documentElement.scrollLeft),
-		// if any scroll is attempted,
-		// set this to the previous value
-		(window.onscroll = function () {
-			window.scrollTo(scrollLeft, scrollTop);
-		});
-
-	function enableScroll() {
-		window.onscroll = function () {};
-	}
-	//document.body.classList.toggle('stop-scrolling');
+	document.body.classList.toggle('stop-scrolling');
 
 	menu.classList.toggle('active');
 	menuImg.classList.toggle('active');
