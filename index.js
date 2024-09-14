e328cbdfe56365076e37f0eefe1c21d76774d2e1
Enzo Cañete <enzodev4@gmail.com>
2024-09-14 15:05:10 -0300

    Divided function that controls the menu and added eventlistener
    into two different functions and a listener to close menu on esc key

--- index.js
+++ index.js
@@ -4,18 +4,37 @@ let menuImg = document.querySelector('.menu-img');
 let menu = document.querySelector('.menu');
 let overlay = document.querySelector('.overlay');
 
-menuBtn.addEventListener('click', function toggleMenu() {
-	// Changes between images depending on the state of the menu
-	if (!menuImg.classList.contains('active')) {
-		menuImg.src = 'images\\icons\\close.svg';
-	} else {
-		menuImg.src = 'images\\icons\\menu-icon.png';
-	}
+function openMenu() {
+	menuImg.src = 'images\\icons\\close.svg';
+	menu.classList.add('active');
+	menuImg.classList.add('active');
 
 	document.body.classList.toggle('stop-scrolling');
+	overlay.classList.add('active');
+}
 
-	menu.classList.toggle('active');
-	menuImg.classList.toggle('active');
+function closeMenu() {
+	menuImg.src = 'images\\icons\\menu-icon.png';
 
-	overlay.classList.toggle('active');
+	menu.classList.remove('active');
+	menuImg.classList.remove('active');
+
+	document.body.classList.remove('stop-scrolling');
+	overlay.classList.remove('active');
+}
+
+menuBtn.addEventListener('click', () => {
+	// Changes between images depending on the state of the menu
+	if (!menuImg.classList.contains('active')) {
+		openMenu();
+	} else {
+		closeMenu();
+	}
+});
+
+/* If the user presses "Esc" and the modal is open, it closes it */
+document.addEventListener('keydown', function (e) {
+	if (e.key === 'Escape' && menu.classList.contains('active')) {
+		closeMenu();
+	}
 });
